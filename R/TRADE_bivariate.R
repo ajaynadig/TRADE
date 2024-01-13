57efbe8b86309b612bdd54a37feb86bded048958
Ajay Nadig <ajay.g.nadig@gmail.com>
2024-01-13 14:14:01 -0500

    added in key TRADE scripts

---
+++ R/TRADE_bivariate.R
@@ -0,0 +1,207 @@
+TRADE_bivariate <- function(results1 = NULL,
+                          results2 = NULL,
+                          genes_exclude = NULL,
+                          estimate_sampling_covariance = FALSE,
+                          covariance_matrix_set,
+                          component_varexplained_threshold = 0,
+                          weight_nocorr = 1,
+                          samples = 0) {
+  start_time = Sys.time()
+
+  #Gene Exclusion
+  if (!is.null(genes_exclude) & any(genes_exclude %in% c(rownames(results1),rownames(results2)))){
+    genes_exclude = genes_exclude[genes_exclude %in% c(rownames(results1),rownames(results2))]
+    results1 = results1[!(rownames(results1) %in% genes_exclude),]
+    results2 = results2[!(rownames(results2) %in% genes_exclude),]
+  } else if (!is.null(genes_exclude) & !(any(genes_exclude %in% c(rownames(results1),rownames(results2))))) {
+    message("No Exclusion Genes in Results")
+  }
+
+  message("Fitting mixture model")
+
+
+  intersect_genes = Reduce(intersect,
+                           list(rownames(results1),
+                                rownames(results2)))
+
+  betahat_df <- cbind(results1$log2FoldChange[match(intersect_genes,rownames(results1))],
+                      results2$log2FoldChange[match(intersect_genes,rownames(results2))])
+
+  betahat_df_complete = betahat_df[complete.cases(betahat_df),]
+  cor_raw = cor(betahat_df_complete[,1],betahat_df_complete[,2])
+
+  se_df <- cbind(results1$lfcSE[match(intersect_genes,rownames(results1))],
+                 results2$lfcSE[match(intersect_genes,rownames(results2))])
+
+  data = mash_set_data(betahat_df, se_df)
+
+  #Generate grid of covariance matrices
+  #First, run univariate TIM to help identify mixture components
+
+  if (covariance_matrix_set == "grid") {
+    slopes = logseq(0.1,10,n=5)
+    corrs = seq(-1,1,length.out = 9)
+    U_grid = list()
+    i = 1
+    for (slope in slopes) {
+      for (corr in corrs) {
+        U_i = diag(2)
+        U_i[1,1] = 1/(slope + 1)
+        U_i[2,2] = slope/(slope + 1)
+        U_i[1,2] = corr * sqrt(U_i[1,1] * U_i[2,2])
+        U_i[2,1] = corr * sqrt(U_i[1,1] * U_i[2,2])
+
+        U_grid[[i]] = U_i
+        i = i + 1
+      }
+    }
+
+    U = U_grid
+
+    if (estimate_sampling_covariance) {
+      message("Estimate sampling correlation")
+      V.em = mash_estimate_corr_em(data, U)
+      data = mash_update_data(data, V=V.em$V)
+    }
+
+    m   = mash(data, U)
+  } else if (covariance_matrix_set == "mash_default") {
+
+    message("Get data-driven covariance matrices")
+    m.1by1 = mash_1by1(data)
+    strong = unique(which.minn(m.1by1$result$lfsr[,1],round(0.05 * nrow(m.1by1$result$lfsr))),
+                    which.minn(m.1by1$result$lfsr[,2],round(0.05 * nrow((m.1by1$result$lfsr)))))
+    U.pca = cov_pca(data,2,subset=strong)
+    U.ed = cov_ed(data, U.pca, subset=strong)
+
+    message("Get canonical covariance matrices")
+    U.c = cov_canonical(data)
+
+
+    U = c(U.c,U.ed)
+
+    if (estimate_sampling_covariance) {
+      message("Estimate sampling correlation")
+      V.em = mash_estimate_corr_em(data, U)
+      data = mash_update_data(data, V=V.em$V)
+    }
+
+    m   = mash(data, U)
+  } else if (covariance_matrix_set == "adaptive_grid") {
+
+    results1_ash <- ash(betahat_df[,1],
+                        se_df[,1],
+                        mixcompdist = "halfnormal",
+                        prior = "uniform")
+
+    results1_sds = unique(results1_ash$fitted_g$sd[-1])
+    results1_sds_weights <- sapply(results1_sds, function(x) sum(results1_ash$fitted_g$pi[results1_ash$fitted_g$sd == x]))
+    #results1_sds_varexplained = (results1_sds_weights * sum(complete.cases(betahat_df))) *(results1_sds^2)
+    #results1_sds_proportionexplained = results1_sds_varexplained/sum(results1_sds_varexplained)
+    results1_vars = (results1_sds^2)[results1_sds_weights > component_varexplained_threshold]
+
+    results2_ash <- ash(betahat_df[,2],
+                        se_df[,2],
+                        mixcompdist = "halfnormal",
+                        prior = "uniform")
+
+    results2_sds = unique(results2_ash$fitted_g$sd[-1])
+    results2_sds_weights <- sapply(results2_sds, function(x) sum(results2_ash$fitted_g$pi[results2_ash$fitted_g$sd == x]))
+    #results2_sds_varexplained = (results2_sds_weights * sum(complete.cases(betahat_df))) *(results2_sds^2)
+    #results2_sds_proportionexplained = results2_sds_varexplained/sum(results2_sds_varexplained)
+    results2_vars = (results2_sds^2)[results2_sds_weights > component_varexplained_threshold]
+
+    U_adaptive_grid = list()
+    i = 1
+    corrs = seq(-1,1,length.out = 9)
+    for (results1_var in c(0,results1_vars)) {
+      for (results2_var in c(0,results2_vars)) {
+        for (corr in corrs) {
+          U_i = diag(2)
+          U_i[1,1] = results1_var
+          U_i[2,2] = results2_var
+          U_i[1,2] = corr * sqrt(U_i[1,1] * U_i[2,2])
+          U_i[2,1] = corr * sqrt(U_i[1,1] * U_i[2,2])
+          U_adaptive_grid[[i]] = U_i
+          i = i + 1
+        }
+      }
+    }
+
+    U = unique(U_adaptive_grid)
+    prior = c(weight_nocorr,
+              sapply(1:length(U),
+                   function(x) {
+                     covarmat = U[[x]]
+                     if (covarmat[1,2] !=0) {return(1)} else {return(weight_nocorr)}
+                   }))
+    if (estimate_sampling_covariance) {
+      message("Estimate sampling correlation")
+      V.em = mash_estimate_corr_em(data, U, grid = 1,normalizeU = FALSE)
+      data = mash_update_data(data, V=V.em$V)
+    }
+
+    m   = mash(data, U, grid = 1,normalizeU = FALSE,prior=prior)
+  }
+
+  message("Run mash")
+
+  message("Compute mixture covariance matrix")
+  covariance_matrices = array(data = NA, dim = c(2,2,length(m$fitted_g$pi)))
+  covariance_matrices[,,1] <- 0
+
+  for (weightnum in 1:length(m$fitted_g$grid)){
+    for (ulistnum in 1:length(U)) {
+      covariance_matrices[,,((weightnum - 1)*length(U)) + ulistnum + 1] <- m$fitted_g$Ulist[ulistnum][[1]] * m$fitted_g$grid[weightnum]
+    }
+  }
+
+
+  weighted_covariance_matrices = array(data = NA, dim = c(2,2,length(m$fitted_g$pi)))
+
+  for (matrixnum in 1:length(m$fitted_g$pi)) {
+    weighted_covariance_matrices[,,matrixnum] <- covariance_matrices[,,matrixnum] * m$fitted_g$pi[matrixnum]
+  }
+
+
+  mixture_covariance_matrix <- rowSums(weighted_covariance_matrices,dims = 2)
+
+  if (any(diag(mixture_covariance_matrix) == 0)) {
+    mixture_correlation_matrix = matrix(NA, nrow = 2, ncol = 2)
+    message("One Perturbation has 0 Effect Size Variance; Correlation not defined")
+  } else {
+    mixture_correlation_matrix = solve(diag(sqrt(diag(mixture_covariance_matrix)))) %*% mixture_covariance_matrix %*% solve(diag(sqrt(diag(mixture_covariance_matrix))))
+  }
+
+  #generate samples
+
+  if (samples > 0) {
+    component = sample(1:length(m$fitted_g$pi), size = samples, prob = m$fitted_g$pi, replace = TRUE)
+
+    component_table = table(component)
+
+    posterior_samples = Reduce(rbind,
+                               lapply(1:length(component_table),
+                               function(i) {
+                                 componentnum = as.numeric(names(component_table))[i]
+                                 mvtnorm::rmvnorm(component_table[i],sigma = covariance_matrices[,,componentnum])
+                               }))
+  } else {
+    posterior_samples = NA
+  }
+
+  #Return output
+  end_time = Sys.time()
+  timediff = difftime(end_time,start_time, units = "mins")
+  return(list(TI_correlation = mixture_correlation_matrix[1,2],
+              correlation_matrix = mixture_correlation_matrix,
+              covariance_matrix = mixture_covariance_matrix,
+              cor_raw = cor_raw,
+              fitted = m$fitted_g,
+              posterior_samples = posterior_samples,
+              V = data$V,
+              loglik = m$loglik,
+              runtime = timediff))
+
+}
+
---
+++ R/TRADE_univariate.R
@@ -0,0 +1,254 @@
+require(ggplot2)
+require(doBy)
+require(ashr)
+
+TRADE_univariate <- function(results = NULL,
+                           annot_table = NULL,
+                           n_sample = 10000,
+                           genes_exclude = NULL) {
+
+  na.filter = !is.finite(results$log2FoldChange) | !is.finite(results$lfcSE) | !is.finite(results$pvalue)
+  num_na = sum(na.filter)
+
+  extreme_log2FC = abs(results$log2FoldChange) > 10
+  num_extreme = sum(extreme_log2FC)
+
+  num_exclude = sum(na.filter | num_extreme)
+
+  results = results[!na.filter & !extreme_log2FC,]
+
+
+  message(paste0(num_na, " genes excluded with NA entries"))
+  message(paste0(num_extreme, " genes excluded with extreme log2FC (abs value > 10)"))
+  message(paste0(nrow(results), " genes retained for analysis"))
+
+  #Gene Exclusion
+  if (!is.null(genes_exclude) & any(genes_exclude %in% rownames(results))){
+
+    genes_exclude = genes_exclude[genes_exclude %in% rownames(results)]
+    results = results[!(rownames(results) %in% genes_exclude),]
+    annot_table = annot_table[!(rownames(annot_table) %in% genes_exclude),]
+  } else if (!is.null(genes_exclude) & !(any(genes_exclude %in% rownames(results)))) {
+    message("No Exclusion Genes in Results")
+  }
+
+  #Set number of samples to be drawn to estimate distribution features
+  if (is.null(n_sample)) {n_sample = nrow(results)}
+  message("Fitting mixture model")
+  main_output = get_distribution_output(results,n_sample, annot_table = annot_table)
+
+  main_output$qc = list(num_na = num_na,
+                        num_extreme = num_extreme,
+                        num_exclude = num_exclude)
+  return(main_output)
+}
+
+fit_ash <- function(l2fc,l2fc_se,min_l2fc,max_l2fc,n_sample) {
+  ash_model = ash(betahat = l2fc,
+                  sebetahat = l2fc_se,
+                  mixcompdist = "halfuniform",
+                  outputlevel = 3,
+                  grange = c(min_l2fc,max_l2fc),
+                  prior = "uniform")
+
+  weights =  ash_model$fitted_g$pi
+  uniform_a = ash_model$fitted_g$a
+  uniform_b = ash_model$fitted_g$b
+
+
+  component = sample(c(1:length(weights)),size = n_sample, prob = weights, replace = TRUE)
+  samples = runif(n_sample, min = uniform_a[component], max = uniform_b[component])
+
+
+  return(list(fit = ash_model,
+              samples = samples))
+}
+
+get_distribution_output <- function(results,n_sample, annot_table = NULL) {
+  min_effsize = min(results$log2FoldChange, na.rm = TRUE)
+  max_effsize = max(results$log2FoldChange, na.rm = TRUE)
+
+  total_output = fit_ash(results$log2FoldChange,
+                         results$lfcSE,
+                         min_effsize,
+                         max_effsize,
+                         n_sample)
+
+  #calculate var and mean
+  means = (total_output$fit$fitted_g$a + total_output$fit$fitted_g$b)/2
+  vars = (1/12) * (total_output$fit$fitted_g$b - total_output$fit$fitted_g$a)^2
+
+  mixture_mean = sum(total_output$fit$fitted_g$pi * means)
+
+  variance_expectation = sum(total_output$fit$fitted_g$pi * (means - mixture_mean)^2)
+  expectation_variance =  sum(total_output$fit$fitted_g$pi * vars)
+
+  mixture_variance = variance_expectation + expectation_variance
+
+  #calculate Me
+  fourthmoment_numerator = (((total_output$fit$fitted_g$b - mixture_mean)^5) - ((total_output$fit$fitted_g$a - mixture_mean)^5))
+  fourthmoment_denominator = (total_output$fit$fitted_g$b - mixture_mean - total_output$fit$fitted_g$a + mixture_mean)
+  fourthmoments = (1/5) * (fourthmoment_numerator/fourthmoment_denominator)
+
+  #Set fourth moment 0 for point mass
+  fourthmoments[1] <- 0
+
+  kappa_numerator = sum(total_output$fit$fitted_g$pi * fourthmoments)
+  kappa_denominator = mixture_variance^2
+
+  kappa = kappa_numerator/kappa_denominator
+
+  Me = 3 * nrow(results)/kappa
+
+  df_viz = data.frame(log2FoldChange = c(results$log2FoldChange,total_output$samples),
+                      distribution = c(rep("Empirical",nrow(results)),
+                                       rep("Inferred",length(total_output$samples))))
+  plot <- ggplot(data = df_viz,
+                 mapping = aes(x = log2FoldChange,
+                               fill = distribution,
+                               y = after_stat(scaled)))+
+    geom_density(alpha = 0.2)+
+    theme_bw()+
+    labs(x = "Log2(FC)")
+
+  #significant genes: Bonferroni
+  sig_filter_Bonferroni = results$pvalue < 0.05/sum(!is.na(results$pvalue))
+  sig_filter_Bonferroni[is.na(sig_filter_Bonferroni)] <- FALSE
+  if (any(sig_filter_Bonferroni)){
+
+    sig_tim_Bonferroni <- frac_subset(results,sig_filter_Bonferroni,!sig_filter_Bonferroni,min_effsize,max_effsize,n_sample)
+    var_sig_Bonferroni = sig_tim_Bonferroni$var_subset
+    var_nonsig_Bonferroni = sig_tim_Bonferroni$var_complement
+    frac_sig_Bonferroni = sig_tim_Bonferroni$frac_subset
+    sig_results_Bonferroni = results[sig_filter_Bonferroni,]
+    num_sig_Bonferroni = sig_tim_Bonferroni$n_subset
+    num_nonsig_Bonferroni = sig_tim_Bonferroni$n_complement
+
+  } else {
+    var_sig_Bonferroni = 0
+    frac_sig_Bonferroni = 0
+    sig_results_Bonferroni = NA
+    num_sig_Bonferroni = 0
+    var_nonsig_Bonferroni = NA
+    num_nonsig_Bonferroni = nrow(results)
+  }
+
+  #significant genes: FDR
+  sig_filter_FDR = p.adjust(results$pvalue, method = "fdr") < 0.05
+  sig_filter_FDR[is.na(sig_filter_FDR)] <- FALSE
+  if (any(sig_filter_FDR)){
+
+    sig_tim_FDR <- frac_subset(results,sig_filter_FDR,!sig_filter_FDR,min_effsize,max_effsize,n_sample)
+    var_sig_FDR = sig_tim_FDR$var_subset
+    var_nonsig_FDR = sig_tim_FDR$var_complement
+    frac_sig_FDR = sig_tim_FDR$frac_subset
+    sig_results_FDR = results[sig_filter_FDR,]
+    num_sig_FDR = sig_tim_FDR$n_subset
+    num_nonsig_FDR = sig_tim_FDR$n_complement
+
+  } else {
+    var_sig_FDR = 0
+    frac_sig_FDR = 0
+    sig_results_FDR = NA
+    num_sig_FDR = 0
+    var_nonsig_FDR = NA
+    num_nonsig_FDR = nrow(results)
+  }
+
+#  gene-set enrichments
+  if (!is.null(annot_table)) {
+    frac_var_annot = sapply(1:ncol(annot_table),
+                        function(annot) {
+                          output_annot =  frac_subset(results,
+                                                      annot_table[,annot] == 1,
+                                                      annot_table[,annot] == 0,
+                                             min_effsize,
+                                             max_effsize,
+                                             n_sample)
+                          return(list(var_sig = output_annot$var_subset,
+                                   frac_sig = output_annot$frac_subset))
+                        })
+
+    frac_genes_annot = sapply(1:ncol(annot_table),
+                              function(annot) {
+                                frac = sum(annot_table[,annot],na.rm = TRUE)/sum(!is.na(annot_table[,annot]))
+                              })
+    annot_output_table = data.frame(annot = colnames(annot_table),
+                                    var = unlist(frac_var_annot[1,]),
+                                    frac_var = unlist(frac_var_annot[2,]),
+                                    frac_genes = frac_genes_annot,
+                                    enrichment = unlist(frac_var_annot[2,])/frac_genes_annot)
+
+  } else {
+    annot_output_table = NA
+  }
+
+  return(list(plot = plot,
+              fit = list(distribution = total_output$fit$fitted_g,
+                         #posteriormeans = total_output$fit$result$PosteriorMean,
+                         loglik = total_output$fit$loglik),
+              distribution_summary = list(mean = mixture_mean,
+                                  variance = mixture_variance,
+                                  Me = Me),
+              significant_genes_Bonferroni = list(significant_gene_results_Bonferroni = sig_results_Bonferroni,
+                                       var_sig_Bonferroni = var_sig_Bonferroni,
+                                       var_nonsig_Bonferroni = var_nonsig_Bonferroni,
+                                       frac_sig_Bonferroni = frac_sig_Bonferroni,
+                                       num_sig_Bonferroni = num_sig_Bonferroni,
+                                       num_nonsig_Bonferroni = num_nonsig_Bonferroni),
+              significant_genes_FDR = list(significant_gene_results_FDR = sig_results_FDR,
+                                                  var_sig_FDR = var_sig_FDR,
+                                           var_nonsig_FDR = var_nonsig_FDR,
+                                                  frac_sig_FDR = frac_sig_FDR,
+                                                  num_sig_FDR = num_sig_FDR,
+                                           num_nonsig_FDR = num_nonsig_FDR),
+              annot_output = annot_output_table))
+}
+
+
+frac_subset <- function(results,filter,filter_complement,min_effsize,max_effsize,n_sample) {
+  subset_output = fit_ash(results$log2FoldChange[filter],
+                       results$lfcSE[filter],
+                       min_effsize,
+                       max_effsize,
+                       n_sample)
+
+  complement_output = fit_ash(results$log2FoldChange[filter_complement],
+                          results$lfcSE[filter_complement],
+                          min_effsize,
+                          max_effsize,
+                          n_sample)
+
+  means_subset = (subset_output$fit$fitted_g$a + subset_output$fit$fitted_g$b)/2
+  vars_subset = (1/12) * (subset_output$fit$fitted_g$b - subset_output$fit$fitted_g$a)^2
+
+  mixture_mean_subset = sum(subset_output$fit$fitted_g$pi * means_subset)
+
+  variance_expectation_subset = sum(subset_output$fit$fitted_g$pi * (means_subset - mixture_mean_subset)^2)
+  expectation_variance_subset = sum(subset_output$fit$fitted_g$pi * vars_subset)
+  mixture_variance_subset = variance_expectation_subset + expectation_variance_subset
+
+  means_complement = (complement_output$fit$fitted_g$a + complement_output$fit$fitted_g$b)/2
+  vars_complement = (1/12) * (complement_output$fit$fitted_g$b - complement_output$fit$fitted_g$a)^2
+
+  mixture_mean_complement = sum(complement_output$fit$fitted_g$pi * means_complement)
+
+  variance_expectation_complement = sum(complement_output$fit$fitted_g$pi * (means_complement - mixture_mean_complement)^2)
+  expectation_variance_complement = sum(complement_output$fit$fitted_g$pi * vars_complement)
+  mixture_variance_complement = variance_expectation_complement + expectation_variance_complement
+
+  var_subset = sum(filter,na.rm = TRUE) * mixture_variance_subset
+  var_complement = sum(filter_complement, na.rm = TRUE) * mixture_variance_complement
+
+  frac_subset = var_subset/(var_subset + var_complement)
+
+  return(list(var_subset = mixture_variance_subset,
+              var_complement = mixture_variance_complement,
+         frac_subset = frac_subset,
+         n_subset = sum(filter),
+         n_complement = sum(filter_complement)))
+}
+
+
+
+
