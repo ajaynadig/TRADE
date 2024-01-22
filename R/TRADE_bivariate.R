64ba2b8297e27e4e40815ee286be9f4b80b9ada0
Ajay Nadig <ajay.g.nadig@gmail.com>
2024-01-22 09:50:51 -0500

    Added in "combined" covariance matrix set mode for bivariate TRADE

--- R/TRADE_bivariate.R
+++ R/TRADE_bivariate.R
@@ -5,7 +5,7 @@ TRADE_bivariate <- function(results1 = NULL,
                           results2 = NULL,
                           genes_exclude = NULL,
                           estimate_sampling_covariance = FALSE,
-                          covariance_matrix_set,
+                          covariance_matrix_set = "combined",
                           component_varexplained_threshold = 0,
                           weight_nocorr = 1,
                           n_sample = NULL) {
@@ -39,84 +39,62 @@ TRADE_bivariate <- function(results1 = NULL,
   data = mash_set_data(betahat_df, se_df)
 
   #Generate grid of covariance matrices
-  #First, run univariate TIM to help identify mixture components
+  #Three modes: mash_default, adaptive_grid, and combined
 
-  if (covariance_matrix_set == "grid") {
-    slopes = logseq(0.1,10,n=5)
-    corrs = seq(-1,1,length.out = 9)
-    U_grid = list()
-    i = 1
-    for (slope in slopes) {
-      for (corr in corrs) {
-        U_i = diag(2)
-        U_i[1,1] = 1/(slope + 1)
-        U_i[2,2] = slope/(slope + 1)
-        U_i[1,2] = corr * sqrt(U_i[1,1] * U_i[2,2])
-        U_i[2,1] = corr * sqrt(U_i[1,1] * U_i[2,2])
-
-        U_grid[[i]] = U_i
-        i = i + 1
-      }
-    }
-
-    U = U_grid
-
-    if (estimate_sampling_covariance) {
-      message("Estimate sampling correlation")
-      V.em = mash_estimate_corr_em(data, U)
-      data = mash_update_data(data, V=V.em$V)
-    }
-
-    m   = mash(data, U)
-  } else if (covariance_matrix_set == "mash_default") {
-
+  if (covariance_matrix_set == "mash_default" | covariance_matrix_set == "combined") {
+    
     message("Get data-driven covariance matrices")
     m.1by1 = mash_1by1(data)
     strong = unique(which.minn(m.1by1$result$lfsr[,1],round(0.05 * nrow(m.1by1$result$lfsr))),
                     which.minn(m.1by1$result$lfsr[,2],round(0.05 * nrow((m.1by1$result$lfsr)))))
     U.pca = cov_pca(data,2,subset=strong)
     U.ed = cov_ed(data, U.pca, subset=strong)
-
+    
     message("Get canonical covariance matrices")
     U.c = cov_canonical(data)
-
-
+    
+    
     U = c(U.c,U.ed)
-
-    if (estimate_sampling_covariance) {
+    
+    if (estimate_sampling_covariance & covariance_matrix_set == "mash_default") {
       message("Estimate sampling correlation")
       V.em = mash_estimate_corr_em(data, U)
       data = mash_update_data(data, V=V.em$V)
     }
-
-    m   = mash(data, U)
-  } else if (covariance_matrix_set == "adaptive_grid") {
-
+    
+    if (covariance_matrix_set == "mash_default") {
+      m   = mash(data, U)
+    }
+    
+  }
+  
+  if (covariance_matrix_set == "adaptive_grid" | covariance_matrix_set == "combined") {
+    
     results1_ash <- ash(betahat_df[,1],
                         se_df[,1],
                         mixcompdist = "halfnormal",
                         prior = "uniform")
-
+    
     results1_sds = unique(results1_ash$fitted_g$sd[-1])
     results1_sds_weights <- sapply(results1_sds, function(x) sum(results1_ash$fitted_g$pi[results1_ash$fitted_g$sd == x]))
     #results1_sds_varexplained = (results1_sds_weights * sum(complete.cases(betahat_df))) *(results1_sds^2)
     #results1_sds_proportionexplained = results1_sds_varexplained/sum(results1_sds_varexplained)
     results1_vars = (results1_sds^2)[results1_sds_weights > component_varexplained_threshold]
-
+    
     results2_ash <- ash(betahat_df[,2],
                         se_df[,2],
                         mixcompdist = "halfnormal",
                         prior = "uniform")
-
+    
     results2_sds = unique(results2_ash$fitted_g$sd[-1])
     results2_sds_weights <- sapply(results2_sds, function(x) sum(results2_ash$fitted_g$pi[results2_ash$fitted_g$sd == x]))
     #results2_sds_varexplained = (results2_sds_weights * sum(complete.cases(betahat_df))) *(results2_sds^2)
     #results2_sds_proportionexplained = results2_sds_varexplained/sum(results2_sds_varexplained)
     results2_vars = (results2_sds^2)[results2_sds_weights > component_varexplained_threshold]
-
+    
     U_adaptive_grid = list()
     i = 1
-    corrs = seq(-1,1,length.out = 9)
+    corrs = seq(-1,1,length.out = 21)
     for (results1_var in c(0,results1_vars)) {
       for (results2_var in c(0,results2_vars)) {
         for (corr in corrs) {
@@ -130,22 +108,43 @@ TRADE_bivariate <- function(results1 = NULL,
         }
       }
     }
-
+    
     U = unique(U_adaptive_grid)
     prior = c(weight_nocorr,
               sapply(1:length(U),
-                   function(x) {
-                     covarmat = U[[x]]
-                     if (covarmat[1,2] !=0) {return(1)} else {return(weight_nocorr)}
-                   }))
+                     function(x) {
+                       covarmat = U[[x]]
+                       if (covarmat[1,2] !=0) {return(1)} else {return(weight_nocorr)}
+                     }))
+    if (estimate_sampling_covariance & covariance_matrix_set == "adaptive_grid") {
+      message("Estimate sampling correlation")
+      V.em = mash_estimate_corr_em(data, U, grid = 1,normalizeU = FALSE)
+      data = mash_update_data(data, V=V.em$V)
+    }
+    
+    if (covariance_matrix_set == "adaptive_grid") {
+      m   = mash(data, U, grid = 1,normalizeU = FALSE,prior=prior)
+    }
+  }
+  
+  if (covariance_matrix_set == "combined") {
+    #manually scale U.c and U.ed
+    grid = autoselect_grid(data, sqrt(2))
+    
+    xU_mashdefault = expand_cov(normalize_Ulist(c(U.c,U.ed)),grid)
+    
+    U = unique(c(xU_mashdefault,U_adaptive_grid))
+    
     if (estimate_sampling_covariance) {
       message("Estimate sampling correlation")
       V.em = mash_estimate_corr_em(data, U, grid = 1,normalizeU = FALSE)
       data = mash_update_data(data, V=V.em$V)
     }
-
-    m   = mash(data, U, grid = 1,normalizeU = FALSE,prior=prior)
+    
+    m = mash(data, U, grid = 1,normalizeU = FALSE)
+    
   }
+  
 
   message("Run mash")
 
